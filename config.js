92bfebaef97faa7169509f3e9ee9ef1688090722
DESKTOP-T526RFM\lenovo <akshatjaiswal521@gmail.com>
2022-12-25 14:47:13 +0530

    added config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongodbUri:
+    "mongodb+srv://akshat:pzND2ZLorTrr2ryc@contacts.ucgowm3.mongodb.net/?retryWrites=true&w=majority",
+  jwtSecret:
+    "OQS7t1X5sZvT6bF9iWfPJI9f8WIuPij4VzAEm4qKB6XewoBqJMYikZSeDgQkPJpKbCbBm18izyCd3RZzFJHbi5DGeT4f4kV0eZ52O+4DN8bUZiPXS2VzUS5rjnCIFsrF9wYyDgcyD4LYJFY+ChrjGPLbmxospThz3PCj62awECoGewz5CQ0eBzVedjKoltklVMXZnHBls420QZx34ey+rkLnJa8cmyU+if0r8Xy9aXGM56lnN+qKXVnPj+wZoqy+04MCi/wU/h9ueifAq7fC/60qRPYaYcuUwhQEkxwVNtfBGHluMBkUzHCmf3mOJ7y/yPAZh5Xh1aBCF7TSdQga9w==",
+};
